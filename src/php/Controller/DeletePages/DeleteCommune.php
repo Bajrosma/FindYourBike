ff244da3f069e3edf99441cfeefc4744fe0ea5cf
Baj <106730095+Bajrosma@users.noreply.github.com>
2025-06-01 13:43:31 +0200

    Update DeleteCommune.php

--- src/php/Controller/DeletePages/DeleteCommune.php
+++ src/php/Controller/DeletePages/DeleteCommune.php
@@ -1,16 +1,15 @@
 <?php
-//starting the session
+//démarer la session
 session_start();
-
 /**
 * Auteur :         Bajro Osmanovic
-* Date :           06.05.2025
-* Description :    fichier php qui permet de supprimer une personne
+* Date :           06.05.2025 → Modif : 28.05.2025
+* Description :    fichier php qui permet de supprimer une commune
 */
-
 //ajoute le fichier qui gère les requette SQL
-require_once('../Model/config.php');
-require_once('../Model/database.php');
-
-// recupère tout les utilisateurs pour la vérification
+require_once('../../Model/config.php');
+require_once('../../Model/database.php');
+// fait appel à la fonction qui supprime la commune
 Database::getInstance()->DeleteOneCommune($_GET["ID"]); 
+// retourne sur la liste des communes
+header("Location: ../../View/Listes/ListeCommunePage.php");
