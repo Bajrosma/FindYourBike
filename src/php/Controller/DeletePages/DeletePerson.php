00a5aba0b6cf03ea49efb119bde07ab45cf3df29
Baj <106730095+Bajrosma@users.noreply.github.com>
2025-06-01 13:42:26 +0200

    Update DeletePerson.php

--- src/php/Controller/DeletePages/DeletePerson.php
+++ src/php/Controller/DeletePages/DeletePerson.php
@@ -1,16 +1,15 @@
 <?php
-//starting the session
+//démarer la session
 session_start();
-
 /**
 * Auteur :         Bajro Osmanovic
-* Date :           06.05.2025
+* Date :           06.05.2025 → Modif : 28.05.2025
 * Description :    fichier php qui permet de supprimer une personne
 */
-
 //ajoute le fichier qui gère les requette SQL
 require_once('../Model/config.php');
 require_once('../Model/database.php');
-
-// recupère tout les utilisateurs pour la vérification
+// supprime la personne selectionner
 Database::getInstance()->DeleteOnePerson($_GET["ID"]); 
+
+header("Location: ../../View/Listes/ListePersonnesPage.php");
