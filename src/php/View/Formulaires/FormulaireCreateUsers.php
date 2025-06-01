591ba6ea170f42cb956712eb692ea380fc776b9f
Baj <106730095+Bajrosma@users.noreply.github.com>
2025-06-01 13:45:11 +0200

    Update FormulaireCreateUsers.php

--- src/php/View/Formulaires/FormulaireCreateUsers.php
+++ src/php/View/Formulaires/FormulaireCreateUsers.php
@@ -29,49 +29,57 @@ $db = Database::getInstance();
         <title>FindYourBike</title>
     </head>
 <body>
-  <div class="container">
-    <a href="../Accueil/AccueilAdmin.php" class="btn-back">← Retour</a>
-    <h1>Création d'un compte</h1>
-    <form action="../../Controller/ChecksFormulaires/FormulaireCreateUserCheck.php" method="post">
-        <div class='form-group row mb-3'>
-            <label foruseName' class='col-sm-4 col-form-label'>Nom de l'utilisateurs</label>
-            <div class='col-sm-8'>
-                <input type='Text' class='form-control' name='useName' id='useName'>
+    <div class="container">
+        <a href="../Accueil/AccueilAdmin.php" class="btn-back">← Retour</a>
+        <h1>Création d'un compte</h1>
+        <form action="../../Controller/ChecksFormulaires/FormulaireCreateUserCheck.php" method="post">
+            <div class='form-group row mb-3'>
+                <label foruseName' class='col-sm-4 col-form-label'>Nom de l'utilisateurs</label>
+                <div class='col-sm-8'>
+                    <input type='Text' class='form-control' name='useName' id='useName'>
+                </div>
             </div>
-        </div>
-        <div class='form-group row mb-3'>
-            <label for='usePassword' class='col-sm-4 col-form-label'>Mot de passe</label>
-            <div class='col-sm-8'>
-                <input type='password' class='form-control' name='usePassword' id='usePassword'>
+            <div class='form-group row mb-3'>
+                <label for='usePassword' class='col-sm-4 col-form-label'>Mot de passe</label>
+                <div class='col-sm-8'>
+                    <input type='password' class='form-control' name='usePassword' id='usePassword'>
+                </div>
             </div>
-        </div>
-        <div class='form-group row mb-3'>
-            <label for='usePasswordConfirm' class='col-sm-4 col-form-label'>Confirmer mot de passe</label>
-            <div class='col-sm-8'>
-                <input type='password' class='form-control' name='usePasswordConfirm' id='usePasswordConfirm'>
+            <div class='form-group row mb-3'>
+                <label for='usePasswordConfirm' class='col-sm-4 col-form-label'>Confirmer mot de passe</label>
+                <div class='col-sm-8'>
+                    <input type='password' class='form-control' name='usePasswordConfirm' id='usePasswordConfirm'>
+                </div>
             </div>
-        </div>
-        <!-- Liste des rôles que le compte peut se voir attribuer -->
-        <div class='form-group row mb-3'>
-            <label for="usePrivilage" class='col-sm-4 col-form-label'>Rôle du compte</label>
-            <div class='col-sm-8'>
-            <select class='form-control' name="usePrivilage" id="usePrivilage">
-                <option value="">Veuillez sélectionné le rôle</option>
-                <option value="0">Consultation</option>
-                <option value="1">Administration</option>
-            </select>
+            <!-- Liste des rôles que le compte peut se voir attribuer -->
+            <div class='form-group row mb-3'>
+                <label for="usePrivilage" class='col-sm-4 col-form-label'>Rôle du compte</label>
+                <div class='col-sm-8'>
+                <select class='form-control' name="usePrivilage" id="usePrivilage">
+                    <option value="">Veuillez sélectionné le rôle</option>
+                    <option value="0">Consultation</option>
+                    <option value="1">Administration</option>
+                </select>
+                </div>
             </div>
-        </div>
-        <?php
-            if(isset( $_SESSION["ErrorMessagePasswordConfirm"]))
-            {
-                echo  $_SESSION["ErrorMessagePasswordConfirm"];
-            }
+            <?php
+                if(isset( $_SESSION["ErrorMessagePasswordConfirm"]))
+                {
+                    echo  $_SESSION["ErrorMessagePasswordConfirm"];
+                }
+            ?>
+            <div class="text-center">
+                <button type="submit" class="btn">Soumettre le formulaire</button>
+            </div>
+        </form>
+    </div>
+    <?php
+        /* suppression des valeurs et des messages d'erreurs
+        foreach ($Champs as $Champ => $_) {
+            unset($_SESSION["ErrorMessage" . ucfirst(str_replace("per", "", $Champ))]);
+            unset($_SESSION[$Champ]);
+        }
+        unset($_SESSION["MessageAdd"]);*/
         ?>
-        <div class="text-center">
-            <button type="submit" class="btn">Soumettre le formulaire</button>
-        </div>
-    </form>
-
 </body>
 </html>
