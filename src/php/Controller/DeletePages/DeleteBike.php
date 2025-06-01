0f84e8a60ceb6975f246e4d7de05a5745ac2d11f
Baj <106730095+Bajrosma@users.noreply.github.com>
2025-06-01 13:39:58 +0200

    Update DeleteBike.php

--- src/php/Controller/DeletePages/DeleteBike.php
+++ src/php/Controller/DeletePages/DeleteBike.php
@@ -1,16 +1,15 @@
 <?php
-//starting the session
+//démarer la session
 session_start();
-
 /**
 * Auteur :         Bajro Osmanovic
-* Date :           06.05.2025
-* Description :    fichier php qui permet de supprimer une personne
+* Date :           06.05.2025 → Modif : 28.05.2025
+* Description :    fichier php qui permet de supprimer un vélo
 */
-
 //ajoute le fichier qui gère les requette SQL
-require_once('../Model/config.php');
-require_once('../Model/database.php');
-
-// recupère tout les utilisateurs pour la vérification
-Database::getInstance()->DeleteOneBike($_GET["ID"]); 
+require_once('../../Model/config.php');
+require_once('../../Model/database.php');
+// fait appel à la fonction qui supprime un vélo
+Database::getInstance()->DeleteOneBike($_GET["ID"]);
+// redirection sur la liste des vélos
+header("Location: ../../View/Listes/ListeBikePage.php");
